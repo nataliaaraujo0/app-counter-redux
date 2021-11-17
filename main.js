f50874aa5575e1a70cd7375a8f46c2a8c6b7f00a
alucard <devjrteste@gmail.com>
2021-11-17 03:23:48 -0300

    refector!: isolando `testes` em outro arquivo

--- main.js
+++ main.js
@@ -9,15 +9,3 @@ const counter = (state = 0, action) => {
 
   return state;
 };
-
-console.assert(counter(0, { type: "INCREMENT" }) === 1);
-
-console.assert(counter(1, { type: "INCREMENT" }) === 2);
-
-console.assert(counter(2, { type: "DECREMENT" }) === 1);
-
-console.assert(counter(5, { type: "DECREMENT" }) === 4);
-
-console.assert(counter(3, { type: "SOMETHING" }) === 3);
-
-console.assert(counter(undefined, {}) === 0);
